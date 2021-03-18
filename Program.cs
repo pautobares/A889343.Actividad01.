689c9efa80bae6a122558ee09b72e9f6fe4fab57
Paula Tobares Otero <paulabelen_@hotmail.com>
2021-03-18 20:18:55 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A889343.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Por favor, ingrese una tecla para salir.");
+            Console.ReadLine();
+        }
+    }
+}
